341cc737fec3ff81e70c24675a3cea5e55c20f44
Tianhang Wang <tianhang.wang@mail.utoronto.ca>
2020-06-10 15:55:49 -0400

    fix some bugs and refactor the code

--- src/Cart/cart.js
+++ src/Cart/cart.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './cart.css';
 import 'antd/dist/antd.css';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 import Button from '@material-ui/core/Button';
-import PropTypes from 'prop-types';
-import { lighten, makeStyles, withStyles} from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import Switch from '@material-ui/core/Switch';
 import TextField from '@material-ui/core/TextField';
-
 import shopIcon from "./../Pic/shopIcon.png"
 
 
@@ -54,17 +41,9 @@ export class Cart extends React.Component {
    }
 
 
-
-
-
    render() {
       var React = require('react');
       var QRCode = require('qrcode.react');
-      const useStyles = makeStyles({
-          table: {
-            minWidth: 650,
-          },
-      });
 
       const rows = [
           this.createData('Purple Cap', "$30.00"),
@@ -113,7 +92,7 @@ export class Cart extends React.Component {
       return (
         <body id='container1'>
         <div id="header">
-            <img class='shopIcon' src={shopIcon}/>
+            <img class='shopIcon' src={shopIcon} alt='Icon is not displayed properly'/>
             <h2 id="headContent1">FASHION SHOP</h2>
             <h4 id="headContent2">MAN</h4>
             <h4 id="headContent3">WOMEN</h4>
@@ -124,7 +103,6 @@ export class Cart extends React.Component {
             <h1 id="content"> CHECKOUT</h1>
         </div>
         <hr className="hr1"/>
-
         <div id="container4">
         <div id="container5">
         <div id="container6">
@@ -166,7 +144,7 @@ export class Cart extends React.Component {
               <br/>
             </div>
             </div>
-            <div id='box2'><Button variant="contained" color="secondary" htmlType="back" htmlType="back" onClick={this.jump.bind(this)}>
+            <div id='box2'><Button variant="contained" color="secondary"  htmlType="back" onClick={this.jump.bind(this)}>
               Go Back
             </Button>
         </div>
@@ -207,8 +185,6 @@ export class Cart extends React.Component {
         <br/>
         <QRCode value="http://facebook.github.io/react/" />
         </div>
-
-
         </body>
         )
     };
--- src/Shop/shop.js
+++ src/Shop/shop.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import "./shop.css"
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import shopIcon from "./../Pic/shopIcon.png"
-import bannerPic from "./../Pic/banner.jpg"
 import hat from "./../Pic/hat.png"
-import hatLogo from "./../Pic/hatLogo.png"
 
 
 export class Shop extends React.Component {
@@ -18,7 +15,7 @@ export class Shop extends React.Component {
         return (
             <body id='container1'>
                 <div id="header">
-                    <img class='shopIcon' src={shopIcon}/>
+                    <img class='shopIcon' src={shopIcon} alt='shopIcon is not displayed properly'/>
                     <h2 id="headContent1">FASHION SHOP</h2>
                     <h4 id="headContent2">MAN</h4>
                     <h4 id="headContent3">WOMEN</h4>
@@ -29,7 +26,7 @@ export class Shop extends React.Component {
                     <h1 id="content"> LAST PARTIES OF 2019</h1>
              	</div>
                 <div className="container1">
-                    <img class='hatPic' src={hat}/> <span/><span/>
+                    <img class='hatPic' src={hat} alt='hatPic is not displayed properly'/> <span/><span/>
                     <h2>PURPLE CAP</h2> <span/>
                     <p>$30</p>
                     <Button variant="contained" color="secondary" htmlType="back" onClick={this.jump.bind(this)}>
